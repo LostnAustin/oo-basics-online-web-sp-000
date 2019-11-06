df619e48a4f351bfc0218840186b93ba8af7bdab
Chad Lengefeld <chadlengefeld@gmail.com>
2019-11-06 17:55:59 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,15 @@
 class Book
-    def initialize(title)
+    def initialize(title,author_name)
       @title = title
+      @author_name = author_name
     end
 
     def title
         @title
+    end
+
+    def author_name
+      @author_name
     end
 end
 book = Book.new("And Then There Were None")
